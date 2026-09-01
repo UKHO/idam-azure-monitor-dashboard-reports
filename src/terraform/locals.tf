9a8b976143301a2cdf06a35d7953f8f5045f7d17
Scott James <scott.james@ukho.gov.uk>
2026-09-01 15:46:12 +0100

    Restructure existing workbook terraform for expansion

--- src/terraform/locals.tf
+++ src/terraform/locals.tf
@@ -1,17 +1,10 @@
 locals {
   environment = lower(var.environment)
 
-  resource_group_name = "pims-reporting-${local.environment}-rg"
-
-  workbook_name = "pims-reporting-${local.environment}-workbook"
-
-  workbook_id = {
-    "dev"  = "c2a4c3ba-e52d-4a43-ad31-1a9bdc1bbb32",
-    "live" = "6b0de277-a58d-45b9-a7f2-019a35520700"
-  }
+  resource_group_name = "azure-monitor-dashboard-reports-${local.environment}-rg"
 
   tags = {
-    SERVICE          = "PIMS Reporting"
+    SERVICE          = "Azure Monitor Dashboard Reports"
     ENVIRONMENT      = title(local.environment)
     RESPONSIBLE_TEAM = "IDAM"
     CALLOUT_TEAM     = "N/A"
